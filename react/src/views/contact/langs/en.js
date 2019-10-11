b5d91e47cbcaa203acd325a9b7acf8dc5661870a
dave1496 <dav_elmaleh@hotmail.com>
2019-10-11 16:28:07 +0200

    feat: Remove contact part

--- react/src/views/contact/langs/en.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: "Contact",
-  mail: "E-mail",
-  nom: "First name",
-  prenom: "Last name",
-  message: "Message",
-  submit: "Submit",
-  msgSent: "Your message has been sent !",
-};
--- react/src/views/contact/langs/fr.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: "Contact",
-  mail: "E-mail",
-  nom: "Nom",
-  prenom: "Prenom",
-  message: "Message",
-  submit: "Envoyer",
-  msgSent: "Votre message a été envoyé !",
-};
