0016d1adb9381a50746bd90620e31fc6b8b6231b
dave1496 <dav_elmaleh@hotmail.com>
2019-10-12 02:04:04 +0200

    feat: Skills part

--- react/src/views/skills/index.js
+++ react/src/views/skills/index.js
@@ -1,31 +1,59 @@
 // React
 import React from 'react';
-import PropTypes from 'prop-types';
-
-// Langs
-// import Lang_en from './langs/en';
-// import Lang_fr from './langs/fr';
+import { Card } from 'reactstrap';
 
 // Style
 import {
-  BackgroundDiv,
-  Container,
-  Title
+    BackgroundDiv,
+    Container,
+    Title,
+    TextDivider,
+    Content,
+    Col,
+    Logo,
 } from './style';
 
-const Skills = props => {
-  // const lang = props.lang === 'fr' ? Lang_fr : Lang_en;
-  return (
+const Skills = () => (
     <BackgroundDiv id="Skills">
-      <Container>
-        <Title>Skills</Title>
-      </Container>
+        <Container>
+            <Title>Skills</Title>
+            <TextDivider>DATA SCIENCE</TextDivider>
+            <Content>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/python.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/numpy.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/scipy.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/opencv.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/scikit.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/tensorflow.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/pytorch.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/keras.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/hadoop.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/spark.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/dialogflow.png' /></Col>
+            </Content>
+            <TextDivider>WEB/MOBILE DEVELOPMENT</TextDivider>
+            <Content>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/html.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/css.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/js.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/reactjs.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/node.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/php.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/flask.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/elasticsearch.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/firebase.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/mysql.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/mongodb.png' /></Col>
+            </Content>
+            <TextDivider>PROJECT MANAGEMENT</TextDivider>
+            <Content>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/github.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/docker.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/gcp.png' /></Col>
+                <Col lg={2} md={3} sm={6}><Logo src='images/skills/aws.png' /></Col>
+            </Content>
+        </Container>
     </BackgroundDiv>
-  );
-}
-
-Skills.propTypes = {
-  lang: PropTypes.node,
-}
+);
 
 export default Skills;
--- react/src/views/skills/style.js
+++ react/src/views/skills/style.js
@@ -1,6 +1,6 @@
 // React requirement
 import { styled } from 'styletron-react';
-import { Container as Cont } from 'reactstrap';
+import { Container as Cont, Row, Col as C } from 'reactstrap';
 
 export const BackgroundDiv = styled('div', () => ({
   minHeight: '40vh',
@@ -26,4 +26,45 @@ export const Title = styled('div', () => ({
   flexDirection: 'column',
   alinItems: 'center',
   justifyContent: 'center',
+}));
+
+export const TextDivider = styled('h4', () => ({
+  alignItems: 'center',
+  display: 'flex',
+  textAlign: 'center',
+  width: '100%',
+  '::after': {
+    borderTop: '.0625em solid',
+    content: '""',
+    flex: '1',
+    margin: '0 .5em',    
+  },
+  '::before': {
+    borderTop: '.0625em solid',
+    content: '""',
+    flex: '1',
+    margin: '0 .5em',    
+  }
+}));
+
+export const Content = styled(Row, () => ({
+  width: '95%',
+  padding: '10px',
+  marginTop: '10px',
+  marginBottom: '20px',
+  backgroundColor: 'rgba(224, 225, 225, 1)',
+  borderRadius: '10px',
+  border: '2px solid',
+}));
+
+export const Col = styled(C, () => ({
+  display: 'flex',
+  padding: '10px',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+export const Logo = styled('img', () => ({
+  width: '100px',
+  height: 'auto',
 }));
